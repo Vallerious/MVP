d33e34b652ee6b64e1d47b5acd4a58aa08e675d7
Valeri <valeri@localhost.localdomain>
2015-08-31 09:32:36 +0300

    testing identation

---
+++ test.js
@@ -0,0 +1,33 @@
+if (true) {
+
+} else {
+
+}
+
+if (true) {
+
+} else {
+
+}
+
+var asd = function asd(a, b) {
+	return a + b;
+};
+
+if (true) {
+
+}
+
+var abv = function (a, b) {
+	var a = 5;
+
+	if (a == 5) {
+		var b = 5;
+	} else {
+
+	}
+
+	if (b == 5) {
+		a = 5;
+	}
+};
